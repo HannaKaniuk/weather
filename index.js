4dae318c4c88a9da428a70a7ff5ad11af40d4112
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2024-09-10 14:43:47 +0300

    key

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const express = require('express');
-const path = require('path');  
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fetch from 'node-fetch';
+import 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+const PORT = 3000;
+const weatherApiKey = process.env.API_KEY;
 
 
 app.use(express.static(path.join(__dirname, '/')));
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/api/weather', async (req, res) => {
+  const { city } = req.query;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric`;
 
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    res.status(500).json({ error: 'Error fetching weather data' });
+  }
+});
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
